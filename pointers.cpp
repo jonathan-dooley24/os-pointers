be0296fb65c39c38d2e3c086c8ae5a55cad6199a
jonathan-dooley24 <dool3142@stthomas.edu>
2021-02-11 18:26:58 -0600

    pointers.cpp finsihed.

--- pointers.cpp
+++ pointers.cpp
@@ -1,12 +1,16 @@
 // compile: g++ -std=c++11 -o pointers pointers.cpp
 #include <iostream>
 #include <string>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sstream>
+#include <iomanip>
 
 typedef struct Student {
     int id;
     char *f_name;
     char *l_name;
-    int n_assignments;
+    int n_assignments; 
     double *grades;
 } Student;
 
@@ -17,13 +21,40 @@ void calculateStudentAverage(void *object, double *avg);
 int main(int argc, char **argv)
 {
     Student student;
-    double average;
+    //allocate space for pointers in struct student
+    student.f_name = new char[128];
+    student.l_name = new char[128];
+    student.grades = new double[10];
 
     // Sequence of user input -> store in fields of `student`
+    student.id = promptInt("Please enter the student's id number: ", 0, 999999999);
+
+    //may need error-checking to ensure data type for user response
+    std::cout << "Please enter the student's first name: ";
+    std::cin >> student.f_name;
+    std::cout << "Please enter the student's last name: ";
+    std::cin >> student.l_name;
+
+    student.n_assignments = promptInt("Please enter how many assignments were graded: ", 1 , 134217728);
+    printf("\n");
+
+    std::string message;
+    for(int i = 0; i < student.n_assignments; i++){
+        std::stringstream message_stream;
+        message_stream << "Please enter grade for assignment " << i << ": "; //build the string stream
+        message = message_stream.str(); //set message to the string stream value
+        student.grades[i] = promptDouble(message, 0.0, 1000.0);
+    }
 
     // Call `CalculateStudentAverage(???, ???)`
-    // Output `average`
+    double average = 0.0; //necessary so that ave_ptr is not null. needs initializaion
+    double* ave_ptr = &average;
+    Student* student_ptr = &student;
+    calculateStudentAverage(student_ptr, ave_ptr);
 
+    // Output `average`
+    std::cout << "\nStudent: " << student.f_name << " " << student.l_name << " [" << student.id << "]\n  Average grade: "
+        << std::fixed << std::setprecision(1) << *ave_ptr << "\n";
     return 0;
 }
 
@@ -34,7 +65,32 @@ int main(int argc, char **argv)
 */
 int promptInt(std::string message, int min, int max)
 {
-    // Code to prompt user for an int
+    std::string response;
+    int int_response = -1; //needs initialization as the return value
+    while(1){
+        int digits = 0; //counter used to verify that response can be converted to an int
+        std::cout << message; //prompt message to console
+        std::cin >> response; //store response
+        for(int i = 0; i < response.length(); i++){ //for each element in string response
+            if(isdigit(response[i])){ //if this element is a digit
+                digits++;
+            }
+        }
+        if(digits == response.length()){ //if response is only digits, can be converted to int
+            int_response = stoi(response);
+
+            if(int_response < min || int_response > max){ //if response was in digits, but out of bounds
+                std::cout << "Sorry, I cannot understand your answer\n";
+            }
+            else{
+                break; //exit loop. response was valid.
+            }     
+        }
+        else{ //response contained non-digits. Unable to convert to int
+            std::cout << "Sorry, I cannot understand your answer\n";
+        }
+    }
+    return int_response;
 }
 
 /*
@@ -42,9 +98,38 @@ int promptInt(std::string message, int min, int max)
    min: minimum value to accept as a valid double
    max: maximum value to accept as a valid double
 */
-double promptDouble(std::string message, double min, double max)
+double promptDouble(std::string message, double min, double max)   
 {
-    // Code to prompt user for a double
+    std::string response;
+    double dbl_response = -1.0; //needs initialization as the return value
+    while(1){
+        int digits = 0; //counter used to verify that response can be converted to an int
+        int decimal_pt = 0;
+        std::cout << message; //prompt message to console
+        std::cin >> response; //store response       
+        for(int i = 0; i < response.length(); i++){ //for each element in string response
+            if(isdigit(response[i])){ //if this element is a digit
+                digits++;
+            }
+            else if((response[i] == '.')  && (i != 0 && i != response.length()-1)){ //else if response element is a decimal pt and not in first or last spot
+                decimal_pt++;
+            }
+        }
+        if((decimal_pt == 0 || decimal_pt == 1) && (digits + decimal_pt == response.length())){ //if response is only digits and at most 1 decimal point, convert to dbl
+            dbl_response = strtod(response.c_str(), nullptr);
+
+            if(dbl_response < min || dbl_response > max){ //if response was in digits, but out of bounds
+                std::cout << "Sorry, I cannot understand your answer\n";
+            }
+            else{
+                break; //exit loop. response was valid.
+            }     
+        }
+        else{ //response contained non-digits. Unable to convert to int
+            std::cout << "Sorry, I cannot understand your answer\n";
+        }
+    }
+    return dbl_response;
 }
 
 /*
@@ -53,5 +138,12 @@ double promptDouble(std::string message, double min, double max)
 */
 void calculateStudentAverage(void *object, double *avg)
 {
-    // Code to calculate and store average grade
+    // void* object points to the mem address of student
+    Student* stu = (Student*)object; //use stu to take mem address of student and get a pointer to student
+    double average = 0.0;
+    for(int i = 0; i < stu->n_assignments; i++){
+        average += stu->grades[i]; //'stu->grades[i]' is equivalent to '*stu.grades[i]'
+    }
+    average = average / stu->n_assignments; //divide by total num to get average
+    *avg = average; //store avg as the mem address of the newly calculated product
 }
